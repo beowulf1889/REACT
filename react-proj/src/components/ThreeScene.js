5d69ae31bcd21e644181a0f6ed528e597a911df2
beowulf1889 <anthonyrasband2939@mtec.edu.stu>
2023-11-15 07:49:49 -0700

    colored cube added lighting

--- react-proj/src/components/ThreeScene.js
+++ react-proj/src/components/ThreeScene.js
@@ -7,13 +7,37 @@ const ThreeScene = () => {
   const canvasRef = useRef();
 
   useEffect(() => {
+   
+    
     const canvas = canvasRef.current;
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
+
+    // First directional light
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight1.position.set(0, 1, 0);
+directionalLight1.castShadow = true;
+scene.add(directionalLight1);
+
+// Second directional light
+const directionalLight2 = new THREE.DirectionalLight(0xff0000, 1); // Red color, lower intensity
+directionalLight2.position.set(1, 0, 0); // Different position
+directionalLight2.castShadow = true;
+scene.add(directionalLight2);
+
+
+const directionalLight3 = new THREE.DirectionalLight(0xff0000, 1); // Red color, lower intensity
+directionalLight2.position.set(0, 0, 1); // Different position
+directionalLight2.castShadow = true;
+scene.add(directionalLight3);
+
+  
+
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableZoom = true;
@@ -24,19 +48,30 @@ const ThreeScene = () => {
     const backgroundTexture = textureLoader.load('');
     scene.background = backgroundTexture;
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial({    color: 0xff0000, // Red color
+    roughness: 0.5,
+    metalness: 0.5,});
+    
     const cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = true; // Enable shadow casting for the cube
     scene.add(cube);
 
+    
     const animate = () => {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      
+      cube.lookAt(camera.position)
+     
+      
       renderer.render(scene, camera);
+      
     };
 
     animate();
+
+    
+
   }, []);
 
   return <canvas ref={canvasRef} />;
--- react-proj/src/pages/index.js
+++ react-proj/src/pages/index.js
@@ -12,13 +12,17 @@ export default function Home() {
     
     <>    
    
+    
+    
     <div className='h-screen w-screen flex object-contain justify-center static'>
-    <input className=' absolute top-96 w-8  text-black z-30 h-3 '></input>
-    <img className='text-black  flex z-20  ' src='/PICTURES/_5792cc54-de7d-4350-ac82-6d21b42daad6.png' alt='computer' />
-    <div className=' flex z-10 absolute   '>
+    
+    
+    
+  
+    <div className=' flex z-40 absolute   '>
       <ThreeScene></ThreeScene>
       </div>
-      
+        
     </div>
 
     
